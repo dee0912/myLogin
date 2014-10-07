bd06bfca4e8e330c193ae0036119b0bb9ddd463b
dee Huang <472323087@qq.com>
2014-10-07 15:21:24 +0800

    delete repeated codes from plugins/function.showval.php
    delete repeated codes from plugins/function.showval.php

--- myLogin/plugins/function.showval.php
+++ myLogin/plugins/function.showval.php
@@ -41,48 +41,5 @@ function smarty_function_showval($params,$smarty){
 	return $mdnum;
 }
 
-<?php
-
-//生成验证码
-function smarty_function_showval($params,$smarty){
-
-	$num = "";
-	for($i=0;$i<4;$i++){
-
-		$tmp = rand(1,15);
-		if ($tmp > 9) {
-			switch ($tmp) {
-				case(10):
-					$num .= 'a';
-					break;
-				case(11):
-					$num .= 'b';
-					break;
-				case(12):
-					$num .= 'c';
-					break;
-				case(13):
-					$num .= 'd';
-					break;
-				case(14):
-					$num .= 'e';
-					break;
-				case(15):
-					$num .= 'f';
-					break;
-			}
-		} else {
-			$num .= $tmp;
-		}	
-	}
-
-	$mdnum = md5($num);
-	$_SESSION['num'] = $num;
-	$_SESSION['mdnum'] = $mdnum;
-
-	//写在session之后
-	return $mdnum;
-}
-
 $_SESSION['num'] = smarty_function_showval($params,$smarty);
 $_SESSION['mdnum'] = md5(smarty_function_showval($params,$smarty));
